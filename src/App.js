903bc69ee10b413138b1c6227c517f31fa821a2a
Kyle Sullivan <kylesullivan123@gmail.com>
2021-07-09 14:27:57 -0700

    big_inversion commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import PlayerCord from './components/PersonCard'
+import PersonCard from './components/PersonFunc';
 
 function App() {
   return (
     <div className="App">
-      <PlayerCord first_name='Jane' last_name='Doe' age={45} hair_color='Black'/>
-      <PlayerCord first_name='John' last_name='Smith' age={88} hair_color='Brown'/>
-      <PlayerCord first_name='Millard' last_name='Fillmore' age={50} hair_color='Brown'/>
-      <PlayerCord first_name='Maria' last_name='Smith' age={62} hair_color='Brown'/>
+      <PersonCard first_name='Jane' last_name='Doe' age={45} hair_color='Black'/>
+      <PersonCard first_name='John' last_name='Smith' age={88} hair_color='Brown'/>
+      <PersonCard first_name='Millard' last_name='Fillmore' age={50} hair_color='Brown'/>
+      <PersonCard first_name='Maria' last_name='Smith' age={62} hair_color='Brown'/>
 
     </div>
   );
--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -5,19 +5,20 @@ import React, {Component} from 'react'
 class PlayerCard extends React.Component {
     constructor (props) {
         super(props)
-    }
+        }
     
 
 
     render(){
         return(
             <div>
-                <h1>{this.props.last_name}, {this.props.first_name}</h1>
+                <h1>{this.props.name}</h1>
                 <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hair_color}</p>
+                <p>Hair Color: {this.props.hair_color}</p>    
             </div>
         )
-    }
+        }
+    
 }
 
 export default PlayerCard
