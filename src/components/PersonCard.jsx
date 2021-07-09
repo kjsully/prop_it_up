eb5f6a8374cdf7f3f8c5a22f227430c2ae90e7fc
Kyle Sullivan <kylesullivan123@gmail.com>
2021-07-09 10:57:57 -0700

    commit

---
+++ src/components/PersonCard.jsx
@@ -0,0 +1,23 @@
+import { render } from '@testing-library/react'
+import React, {Component} from 'react'
+
+
+class PlayerCard extends React.Component {
+    constructor (props) {
+        super(props)
+    }
+    
+
+
+    render(){
+        return(
+            <div>
+                <h1>{this.props.last_name}, {this.props.first_name}</h1>
+                <p>Age: {this.props.age}</p>
+                <p>Hair Color: {this.props.hair_color}</p>
+            </div>
+        )
+    }
+}
+
+export default PlayerCard
